d4c4e3fc86b92eb8801b43245db787e5da761739
Aditya Sharma <116197864+iamadityasharma92@users.noreply.github.com>
2026-04-25 17:31:52 +0530

    Add: split-array-largest-sum - submission-0

---
+++ Data Structures & Algorithms/split-array-largest-sum/submission-0.java
@@ -0,0 +1,39 @@
+class Solution {
+    public int splitArray(int[] nums, int k) {
+        int n=nums.length;
+        if(n<k){
+            return -1;
+        }
+        int l=0,r=0;
+        for(int num:nums){
+            l=Math.max(l,num);
+            r+=num;
+        }
+        int res=-1;
+        while(l<=r){
+            int m = l+((r-l)/2);
+            if(canSplit(nums,m,k)){
+                res=m;
+                r=m-1;
+            }else{
+                l=m+1;
+            }
+        }
+        return res;
+    }
+
+    public boolean canSplit(int[] nums,int m, int k){
+        int sum=0,c=1;
+        for(int i=0;i<nums.length;i++){
+            sum+=nums[i];
+            if(sum>m){
+                c++;
+                sum=nums[i];
+            }
+            if(c>k){
+                return false;
+            }
+        }
+        return true;
+    }
+}
