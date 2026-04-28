2fabb2045cefe5d54918c6956be1fc93b2d4b4f8
Aditya Sharma <116197864+iamadityasharma92@users.noreply.github.com>
2026-04-28 07:37:45 +0530

    Update: reverse-linked-list-ii - submission-0

--- Data Structures & Algorithms/reverse-linked-list-ii/submission-0.java
+++ Data Structures & Algorithms/reverse-linked-list-ii/submission-0.java
@@ -14,12 +14,14 @@ class Solution {
         ListNode dummy=new ListNode(-100);
         dummy.next=head;
 
-        ListNode pre=dummy;
+        ListNode pre=dummy;// start from one position back (in case if l=1 and we need to reverse from first position)
         for(int i=1;i<l;i++){
             pre=pre.next;
         }
-        ListNode prevNode=pre.next;
-        ListNode prev=null;
+        // store previous node for chaining after reversing
+        ListNode prevNode=pre.next;// prevNode have 3 so that we can connect last valli list with this 
+        ListNode prev=null;// previous empty node for reversing
+        // for iterating and reversing
         ListNode current= pre.next;
         for(int i=1;i<=r-l+1;i++){
             ListNode tmp =current.next;
@@ -27,8 +29,9 @@ class Solution {
             prev=current;
             current=tmp;
         }
-        pre.next=prev;
-        prevNode.next=current;
+        // pre have node of first part from where we broke or start reversing list  head = [1,2,3,4,5,6], left = 3, right = 5, it stores 2
+        pre.next=prev;// prev have head of reversed list 
+        prevNode.next=current;// connecting last valli list with end of reversed list -> 3->6 = 1->2->5->4->'3->6'
         return dummy.next;
 
     }
